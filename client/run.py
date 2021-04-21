8bc75926b24462595bbcb86c83e897a4510340d5
Joshua Hawking <joshuahawking1@gmail.com>
2021-04-21 21:00:39 +0100

    Updated client to use API.

--- client/run.py
+++ client/run.py
@@ -30,10 +30,7 @@ task_input = response.json()
 logger = logging.getLogger("logger")
 logger.setLevel(logging.DEBUG)
 
-http_handler = JSONHTTPHandler(
-    "http://172.17.48.1:8000",
-    f"/log/{task_input['_id']}"
-)
+http_handler = JSONHTTPHandler("http://172.17.48.1:8000", f"/log/{task_input['_id']}")
 http_handler.setLevel(logging.DEBUG)
 logger.addHandler(http_handler)
 
@@ -46,7 +43,7 @@ application.start(r"C:\Users\piav\Documents\FileZilla.exe")
 
 try:
     application.wait_for_process_exit(timeout=3)
-    
+
     # If the application has already exited, they may have spawned another process, therefore we'll try and find the
     # application again before erroring
     application.connect(best_match=application_name)
@@ -80,14 +77,29 @@ for proc in psutil.process_iter():
     if proc.name() == "fibratus.exe":
         proc.kill()
 
-# Now execute task_input 
+# Now execute task_input
 for index, stage in enumerate(task_input["precursors"]):
-    print("Now executing precursor {} of {}.".format(index + 1, len(task_input["precursors"])))
+    print(
+        "Now executing precursor {} of {}.".format(
+            index + 1, len(task_input["precursors"])
+        )
+    )
 
     # If this is the final precursor, then we should start tracking registry changes
     if index + 1 == len(task_input["precursors"]):
         print("Starting fibratus capturing...")
-        fibratus_process = subprocess.Popen(["fibratus", "run", "ps.name = 'FileZilla.exe' and kevt.category in ('net','file','registry')", "-f", "capture", "--filament.path", os.getcwd()], cwd=os.getcwd())
+        fibratus_process = subprocess.Popen(
+            [
+                "fibratus",
+                "run",
+                "ps.name = 'FileZilla.exe' and kevt.category in ('net','file','registry')",
+                "-f",
+                "capture",
+                "--filament.path",
+                os.getcwd(),
+            ],
+            cwd=os.getcwd(),
+        )
 
     # Find the control referenced
     control_reference = application.window(**stage["reference"], top_level_only=False)
@@ -96,14 +108,18 @@ for index, stage in enumerate(task_input["precursors"]):
     try:
         control = control_reference.wait("visible", timeout=timeout)
     except pywinauto.timings.TimeoutError:
-        print("Control {} was not available after {} seconds.".format(stage["reference"], timeout))
+        print(
+            "Control {} was not available after {} seconds.".format(
+                stage["reference"], timeout
+            )
+        )
         # TODO: Error handling for when it doesn't go as expected...
-    
+
     # Now run the action defined on the control (executes control.method(parameters))
     if stage["action"]["method"] == "set_toggle_state":
-        current_state = bool(control.get_toggle_state()) 
+        current_state = bool(control.get_toggle_state())
         desired_state = stage["action"]["parameters"]["state"]
-        
+
         if current_state != desired_state:
             control.click()
     else:
@@ -124,7 +140,10 @@ if progress_bar.exists():
     progress_bar_value = progress_bar.legacy_properties().get("Value", "100%")
     while progress_bar_value != "100%":
         progress_bar_value = progress_bar.legacy_properties().get("Value", "100%")
-        print("Waiting for progress bar to reach 100%, current value: ", progress_bar_value)
+        print(
+            "Waiting for progress bar to reach 100%, current value: ",
+            progress_bar_value,
+        )
         time.sleep(5)
 
 if fibratus_process is not None:
@@ -144,15 +163,19 @@ if fibratus_process is not None:
 
 interactive_control_types = [uia_controls.ButtonWrapper]
 
+
 def is_interactive_control(control):
     print(control)
     print(repr(control))
-    
+
     # Check whether it's not inaccessible due to it's parent being blocked by modal dialog
     window_state = get_window_interaction_state(control.parent())
-    if window_state is not None and window_state == WindowInteractionState.BlockedByModalWindow:
+    if (
+        window_state is not None
+        and window_state == WindowInteractionState.BlockedByModalWindow
+    ):
         return False
-    
+
     # Check whether it's listed as  a interactive_control_type
     if type(control) not in interactive_control_types:
         return False
@@ -160,23 +183,22 @@ def is_interactive_control(control):
     # Check it's parent is not the title bar (minimise, maximise, etc.) and buttons from a scrollbar
     if control.parent().friendly_class_name() in ["ScrollBar", "TitleBar"]:
         return False
-    
+
     # Although not great, for now, we can attempt to ignore file buttons
     if "Browse" in control.texts():
         return False
 
     return True
 
+
 def get_debug_info(control):
-    return {
-        "text": control.texts()
-    }
+    return {"text": control.texts()}
+
 
 def attempt_unique_id(control):
     if control.automation_id() != "":
-        return {
-            "auto_id": control.automation_id()
-        }
+        return {"auto_id": control.automation_id()}
+
 
 # Now we've executed all input precusors, check whether the application is still available
 # For example, clicking a "Cancel" button may close down the application
@@ -186,11 +208,13 @@ if not application.is_process_running():
     output = {
         "input_id": task_input["_id"],
         "application_alive": False,
-        "program_installed": False
+        "program_installed": False,
     }
 
     # Check whether the program has successfully installed, or whether it's just been closed...
-    output["program_installed"] = full_install_name in map(lambda software: software["name"], get_installed_software())
+    output["program_installed"] = full_install_name in map(
+        lambda software: software["name"], get_installed_software()
+    )
 else:
     print("Starting window enumeration...")
     enumeration = application.windows(top_level_only=False, enabled_only=True)
@@ -201,32 +225,39 @@ else:
         "input_id": task_input["_id"],
         "application_alive": True,
         "top_window_texts": sorted(application.top_window().children_texts()),
-        "found_controls": []
+        "found_controls": [],
     }
 
     # Now we need to output the list of possible controls
     for control in interactive_controls:
         control_type = None
 
-        output["found_controls"].append({
-            "type": control.friendly_class_name(),
-            "reference": attempt_unique_id(control),
-            "_debug": get_debug_info(control)
-        })
+        output["found_controls"].append(
+            {
+                "type": control.friendly_class_name(),
+                "reference": attempt_unique_id(control),
+                "_debug": get_debug_info(control),
+            }
+        )
 
     # Finally, kill the application
     application.kill(soft=False)
 
-# If a fiberatus_output.json exists, rename it
+fibratus_output = {"reg": [], "file": [], "net": []}
+
 try:
-    os.rename("fibratus_capture.json", f"../server/output/{task_input['_id']}_fibratus.json")
+    with open("fibratus_capture.json", "r") as fibratus_output_file:
+        fibratus_output = json.load(fibratus_output_file)
+    os.remove("fibratus_output.json")
 except FileNotFoundError:
     print("No Fiberatus output found")
 
-# Write our output
-with open(f"../server/output/{task_input['_id']}.json", "w") as task_output_file:
-    json.dump(output, task_output_file, indent=2)
+response = requests.post(
+    API_URL + "/submit_task/" + task_input["_id"],
+    json={"window_enumeration": output, "kernel_events": fibratus_output},
+)
 
-# Once our application has been killed, create a .complete file to inform the server we've finished
-with open(f"../server/output/{task_input['_id']}.complete", "w") as complete_file:
-    complete_file.write("Done!")
+print(response.text)
+
+if response.status_code == 200:
+    print("Successfully posted data")
