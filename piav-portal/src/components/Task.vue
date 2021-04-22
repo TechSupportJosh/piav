77b655fa22a8eb6ce4fd785179f7c6fe2740c085
Joshua Hawking <joshuahawking1@gmail.com>
2021-04-22 16:25:18 +0100

    Added task components to view.

--- piav-portal/src/components/Task.vue
+++ piav-portal/src/components/Task.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="container">
     <h1 class="mb-4">Task {{ taskId }}</h1>
+
+    <div v-if="taskOutput">
+      <h2>Window Enumeration</h2>
+      <window-enumeration :data="taskOutput.window_enumeration"></window-enumeration>
+      <h2>Network Traffic</h2>
+      <network :network-events="taskOutput.kernel_events.net"></network>
+      <h2>Registry Events</h2>
+      <registry :registry-events="taskOutput.kernel_events.registry"></registry>
+    </div>
     {{ taskOutput }}
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, onBeforeUnmount, ref } from "vue";
+import { defineComponent } from "vue";
 import { useRouter } from "../plugins/router";
-import { TaskOutput } from "../typings/TaskOutput";
-import API from "../utils/api";
+import Network from "./Network.vue";
+import Registry from "./Registry.vue";
+import WindowEnumeration from "./WindowEnumeration.vue";
 
 export default defineComponent({
+  components: {
+    Network,
+    WindowEnumeration,
+    Registry,
+  },
   setup: () => {
     const router = useRouter();
 
@@ -19,6 +34,7 @@ export default defineComponent({
 
     let pollInterval: number | null = null;
 
+    /*
     const taskOutput = ref<TaskOutput>();
 
     const pollData = async () => {
@@ -35,6 +51,7 @@ export default defineComponent({
     onBeforeUnmount(() => {
       if (pollInterval) clearInterval(pollInterval);
     });
+*/
 
     return {
       taskId,
