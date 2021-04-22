e01501a94c5d5b7b399be2410818443462260621
Joshua Hawking <joshuahawking1@gmail.com>
2021-04-22 17:58:02 +0100

    Add task input API request.

--- piav-portal/src/utils/api.ts
+++ piav-portal/src/utils/api.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 import { QueueEntry } from "../typings/Queue";
+import { TaskInput } from "../typings/TaskInput";
 import { TaskOutput } from "../typings/TaskOutput";
 
 const client = axios.create({
-  // baseURL: process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8000",
   baseURL: "http://localhost:8000/portal",
   validateStatus: undefined,
 });
 
 const getQueuedMachines = async () => {
   const response = await client.get<QueueEntry[]>("/queue");
+
+  if (response.status === 200) return response.data;
+};
+
+const getTaskInput = async (taskId: string) => {
+  const response = await client.get<TaskInput>(`/task/${taskId}/input`);
 
   if (response.status === 200) return response.data;
 };
@@ -22,5 +28,6 @@ const getTaskOutput = async (taskId: string) => {
 
 export default {
   getQueuedMachines,
+  getTaskInput,
   getTaskOutput,
 };
