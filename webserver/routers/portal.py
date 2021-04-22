781b0573b779e57d8519d88aba60e1b22a463630
Joshua Hawking <joshuahawking1@gmail.com>
2021-04-22 23:21:05 +0100

    Fix raising HTTP errors and returning content on 201 No Content.

--- webserver/routers/portal.py
+++ webserver/routers/portal.py
@@ -36,7 +36,7 @@ async def get_task_input(
     response = await db.input.find_one({"_id": ObjectId(task_id)})
 
     if response is None:
-        return HTTPException(status_code=404, detail="Task does not exist.")
+        raise HTTPException(status_code=404, detail="Task does not exist.")
 
     return response
 
@@ -45,7 +45,7 @@ async def get_task_input(
     "/task/{task_id}/output",
     responses={
         200: {"description": "Task output is returned.", "model": models.TaskOutput},
-        204: {"description": "Task exists but has not finished."},
+        202: {"description": "Task exists but has not finished."},
         404: {"description": "Task does not exist."},
     },
     name="Retrieve task output",
@@ -58,15 +58,13 @@ async def get_task_output(
     response = await db.input.find_one({"_id": ObjectId(task_id)})
 
     if response is None:
-        return HTTPException(status_code=404, detail="Task does not exist.")
+        raise HTTPException(status_code=404, detail="Task does not exist.")
 
     response = await db.output.find_one({"_id": ObjectId(task_id)}, {"_id": 0})
 
     # Return no content if the task exists but has no output
     if response is None:
-        return HTTPException(
-            status_code=204, detail="Task exists but has not finished."
-        )
+        raise HTTPException(status_code=202, detail="Task exists but has not finished.")
 
     return response
 
