371a1c13c3d49809cf0d7b571e61debbe3730e1f
Joshua Hawking <joshuahawking1@gmail.com>
2021-04-22 23:28:03 +0100

    Converted log messages to use {} on client.

--- client/JSONHTTPHandler.py
+++ client/JSONHTTPHandler.py
@@ -1,6 +1,33 @@
 import logging
 import requests
 
+from inspect import getargspec
+
+# https://stackoverflow.com/a/24683360from inspect import getargspec
+class BraceMessage(object):
+    def __init__(self, fmt, args, kwargs):
+        self.fmt = fmt
+        self.args = args
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return str(self.fmt).format(*self.args, **self.kwargs)
+
+class StyleAdapter(logging.LoggerAdapter):
+    def __init__(self, logger):
+        self.logger = logger
+
+    def log(self, level, msg, *args, **kwargs):
+        if self.isEnabledFor(level):
+            msg, log_kwargs = self.process(msg, kwargs)
+            self.logger._log(level, BraceMessage(msg, args, kwargs), (), 
+                    **log_kwargs)
+
+    def process(self, msg, kwargs):
+        return msg, {key: kwargs[key] 
+                for key in getargspec(self.logger._log).args[1:] if key in kwargs}
+
+
 class JSONHTTPHandler(logging.Handler):
     def __init__(self, host, endpoint):
         super(JSONHTTPHandler, self).__init__()
@@ -10,7 +37,7 @@ class JSONHTTPHandler(logging.Handler):
 
     def emit(self, record):
         print(record)
-        requests.post(self.host + self.endpoint, json={
-            "message": record.getMessage(),
-            "levelno": record.levelno
-        })
+        requests.post(
+            self.host + self.endpoint,
+            json={"message": record.getMessage(), "levelno": record.levelno},
+        )
--- client/run.py
+++ client/run.py
@@ -15,7 +15,7 @@ from WindowInteractionState import WindowInteractionState, get_window_interactio
 
 import logging
 import requests
-from JSONHTTPHandler import JSONHTTPHandler
+from JSONHTTPHandler import JSONHTTPHandler, StyleAdapter
 
 API_URL = "http://172.19.112.1:8000/vm"
 
@@ -29,12 +29,15 @@ if response.status_code == 404:
 
 task_input = response.json()
 
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("logger")
-logger.setLevel(logging.DEBUG)
 
 http_handler = JSONHTTPHandler(API_URL, f"/log/{task_input['_id']}")
 http_handler.setLevel(logging.DEBUG)
+
 logger.addHandler(http_handler)
+
+logger = StyleAdapter(logger)
 
 logger.info("Received task, starting application...")
 
@@ -45,13 +48,15 @@ full_install_name = "FileZilla Client 3.52.2"
 application = pywinauto.Application(backend="uia")
 application.start(r"C:\Users\piav\Documents\FileZilla.exe")
 
+
 def get_screenshot_base64():
     # Take image of the current top window
     # https://stackoverflow.com/a/31826470
     image = application.top_window().capture_as_image()
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
-    return base64.b64encode(buffered.getvalue()).decode("utf-8") 
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
 
 try:
     application.wait_for_process_exit(timeout=3)
@@ -60,17 +65,14 @@ try:
     # application again before erroring
     application.connect(best_match=application_name)
 
-    print(application)
 except pywinauto.timings.TimeoutError:
-    print("Process is still alive.")
-
-print(application)
+    logger.debug("Process is still alive.")
 
 # Now attempt to find a top window, if not, we may have the same case as above
 try:
     top_window = application.top_window()
 except RuntimeError:
-    print("Failed to find top window, attempting to reconnect to the application")
+    logger.info("Failed to find top window, attempting to reconnect to the application")
 
     # If the application has already exited, they may have spawned another process, therefore we'll try and find the
     # application again before erroring
@@ -94,15 +96,13 @@ base64_images.append(get_screenshot_base64())
 
 # Now execute task_input
 for index, stage in enumerate(task_input["precursors"]):
-    print(
-        "Now executing precursor {} of {}.".format(
-            index + 1, len(task_input["precursors"])
-        )
+    logger.info(
+        "Now executing precursor {} of {}.", index + 1, len(task_input["precursors"])
     )
 
     # If this is the final precursor, then we should start tracking registry changes
     if index + 1 == len(task_input["precursors"]):
-        print("Starting fibratus capturing...")
+        logger.info("Starting fibratus capturing...")
         fibratus_process = subprocess.Popen(
             [
                 "fibratus",
@@ -123,7 +123,7 @@ for index, stage in enumerate(task_input["precursors"]):
     try:
         control = control_reference.wait("visible", timeout=timeout)
     except pywinauto.timings.TimeoutError:
-        print(
+        logger.warn(
             "Control {} was not available after {} seconds.".format(
                 stage["reference"], timeout
             )
@@ -141,7 +141,7 @@ for index, stage in enumerate(task_input["precursors"]):
         getattr(control, stage["action"]["method"])(**stage["action"]["parameters"])
 
     delay = stage.get("delay_after_action", 0)
-    print("Waiting for {} seconds.".format(delay))
+    logger.debug("Waiting for {} seconds.", delay)
     time.sleep(delay)
 
     # After running this precursor, take a screenshot
@@ -158,14 +158,14 @@ if progress_bar.exists():
     progress_bar_value = progress_bar.legacy_properties().get("Value", "100%")
     while progress_bar_value != "100%":
         progress_bar_value = progress_bar.legacy_properties().get("Value", "100%")
-        print(
-            "Waiting for progress bar to reach 100%, current value: ",
+        logger.info(
+            "Waiting for progress bar to reach 100%, current value: {}",
             progress_bar_value,
         )
         time.sleep(5)
 
 if fibratus_process is not None:
-    print("Stopping fibratus...")
+    logger.info("Stopping fibratus...")
     # Send CTRL+C to the fibratus process to stop it
     # However, sending CTRL+C to this subprocess also sends it to this process
     # Therefore, we should wait for the signal, trap it and ignore it. The subprocess
@@ -174,10 +174,10 @@ if fibratus_process is not None:
         fibratus_process.send_signal(signal.CTRL_C_EVENT)
         time.sleep(60)
     except KeyboardInterrupt:
-        print("Captured KeyboardInterrupt (SIGINT)")
+        logger.debug("Captured KeyboardInterrupt (SIGINT)")
 
     time.sleep(15)
-    print("Fibratus stopped")
+    logger.debug("Fibratus stopped")
 
 interactive_control_types = [uia_controls.ButtonWrapper]
 
@@ -221,12 +221,12 @@ def attempt_unique_id(control):
 # Now we've executed all input precusors, check whether the application is still available
 # For example, clicking a "Cancel" button may close down the application
 if not application.is_process_running():
-    print("Application's process has ended.")
+    logger.info("Application's process has ended.")
 
     output = {
         "application_alive": False,
         "program_installed": False,
-        "base64_images": base64_images
+        "base64_images": base64_images,
     }
 
     # Check whether the program has successfully installed, or whether it's just been closed...
@@ -234,10 +234,13 @@ if not application.is_process_running():
         lambda software: software["name"], get_installed_software()
     )
 else:
-    print("Starting window enumeration...")
+    # Take a final image
+    base64_images.append(get_screenshot_base64())
+
+    logger.info("Starting window enumeration...")
     enumeration = application.windows(top_level_only=False, enabled_only=True)
     interactive_controls = filter(is_interactive_control, enumeration)
-    print("Finished window enumeration...")
+    logger.info("Finished window enumeration...")
 
     output = {
         "application_alive": True,
@@ -269,14 +272,13 @@ try:
         fibratus_output = json.load(fibratus_output_file)
     os.remove("fibratus_output.json")
 except FileNotFoundError:
-    print("No Fiberatus output found")
+    logger.warn("No Fiberatus output found")
 
+logger.info("Uploading data to server...")
 response = requests.post(
     API_URL + "/submit_task/" + task_input["_id"],
     json={"window_enumeration": output, "kernel_events": fibratus_output},
 )
 
-print(response.text)
-
 if response.status_code == 200:
-    print("Successfully posted data")
+    logger.info("Successfully posted data, shutting down...")
